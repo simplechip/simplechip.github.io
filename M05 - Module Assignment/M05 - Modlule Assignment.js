a9ab96d492817a7693866a0f4e6a09ac787cccc3
simplechip <97199735+simplechip@users.noreply.github.com>
2022-01-28 14:42:26 -0800

    Update M05 - Modlule Assignment.js

--- M05 - Module Assignment/M05 - Modlule Assignment.js
+++ M05 - Module Assignment/M05 - Modlule Assignment.js
@@ -1,14 +1,12 @@
 var table = prompt();             // Unit of table
-var operator = 'multiplication'; // Type of calculation
+
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
 
-if (operator === 'multiplication') {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+// Do multiplication
+while (i < 11) {
+  msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+  i++;
 }
 
 // Write the message into the page
